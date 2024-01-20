d62756f7940342ddf2cbcde5d1115c5dadd28e2a
Lemetter Sebastien (XC-AS/EAF3-ADA) <Sebastien.Lemetter@de.bosch.com>
2024-01-20 22:31:40 +0100

    Feature: use ssh protocol, forced by the test framework

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -10,7 +10,7 @@ AESD_ASSIGNMENTS_VERSION = efee700dd6fb66c71c38af93d79e9457abd0a332
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = https://github.com/cu-ecen-aeld/assignments-3-and-later-sebaleme.git
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-sebaleme.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
