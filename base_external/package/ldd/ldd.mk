8d10eda286dde54e3955ada2c914882dd7aa87bb
Sebastien Lemetter <sebaleme@gmail.com>
2024-11-10 14:46:15 +0100

    Feature: try to fix build of skull and misc packages

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -6,6 +6,8 @@
 ##############################################################
 
 # Adding assignment-7 content
+# Read https://www.kernel.org/doc/html/v5.6/kbuild/modules.html for learning how to build a kernel module
+$(info    Building LDD repo packages...)
 LDD_VERSION = e712a1f17c55bbbc22d775f584bc4ad239c1d2ea
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
@@ -13,11 +15,10 @@ LDD_VERSION = e712a1f17c55bbbc22d775f584bc4ad239c1d2ea
 LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-sebaleme.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
+LDD_MODULE_SUBDIRS = scull/
+LDD_MODULE_SUBDIRS += misc-modules/
 
-define LDD_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/misc-modules all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/scull all
-endef
+LDD_MODULE_MAKE_OPTS = KERNELDIR=$(LINUX_DIR)
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 # The -m option sets the mode (access-control bits) of the installed object(s)
@@ -34,4 +35,5 @@ define LDD_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/skull/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S98lddmodules
 endef
 
-$(eval $(generic-package))
+$(eval $(generic-package), LDD)
+$(info    End of LDD build...)
